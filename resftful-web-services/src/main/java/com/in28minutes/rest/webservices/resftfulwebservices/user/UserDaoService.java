4713f6641d34f06219fd3cefefaf7d26ced94983
dang417 <dndeo10814@gmail.com>
2024-05-15 15:18:50 +0900

    REST API CRUD

---
+++ resftful-web-services/src/main/java/com/in28minutes/rest/webservices/resftfulwebservices/user/UserDaoService.java
@@ -0,0 +1,40 @@
+package com.in28minutes.rest.webservices.resftfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static Integer userCounter = 0;
+
+    static {
+        users.add(new User(++userCounter, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCounter, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++userCounter, "Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        user.setId(++userCounter);
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
+}
