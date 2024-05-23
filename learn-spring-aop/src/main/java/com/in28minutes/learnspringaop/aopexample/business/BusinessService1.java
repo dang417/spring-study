8234816f2db60a9dd8dcf510e10cb61804dc6d0e
dang417 <dndeo10814@gmail.com>
2024-05-23 12:10:21 +0900

    spring-aop, aop annotations

---
+++ learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/aopexample/business/BusinessService1.java
@@ -0,0 +1,31 @@
+package com.in28minutes.learnspringaop.aopexample.business;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.in28minutes.learnspringaop.aopexample.annotations.TrackTime;
+import com.in28minutes.learnspringaop.aopexample.data.DataService1;
+
+@Service
+public class BusinessService1 {
+  private DataService1 dataService1;
+
+  public BusinessService1(DataService1 dataService1) {
+    this.dataService1 = dataService1;
+  }
+
+  @TrackTime
+  public int calculateMax() {
+    int[] data = dataService1.retrieveData();
+    // throw new RuntimeException("Something Went Wrong!");
+
+    try {
+      Thread.sleep(30);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    return Arrays.stream(data).max().orElse(0);
+  }
+}
